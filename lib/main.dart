02c7fc066d8bd1958eed22f2dab87165aebd67a7
Anil Sharam <anilgsharma900@gmail.com>
2023-10-22 20:35:46 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.black,
       ),
       home: const MyHomePage(title: 'Bottom Navigation Bar'),
     );
