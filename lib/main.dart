6c335ca161e629261ca51b852116fa3df802d961
Anil Sharam <anilgsharma900@gmail.com>
2023-10-22 20:38:37 +0530

    Update main.dart
    jenkins webhook

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.black,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Bottom Navigation Bar'),
     );
